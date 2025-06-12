75ee6898e2beff4ea543d05297d6325204a74aa9
Anirut <vobi69p@gmail.com>
2025-06-12 22:00:58 +1200

    feat: User can click Home button on Navbar to go to homepage. Display different color for button that match with current page.

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,9 +1,13 @@
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { ThemeToggleButton } from "@/components/Theme-toggle-button";
+import { Button } from "@/components/ui/button";
 
 export default function Navbar() {
+    const pathname = usePathname();
+
     return (
         <nav className="bg-muted text-foreground px-4 py-2 shadow-md dark:bg-gray-900">
             <div className="max-w-6xl mx-auto flex justify-between items-center">
@@ -11,8 +15,36 @@ export default function Navbar() {
                     AI Resume Analyzer
                 </Link>
                 <div className="flex gap-4 text-sm items-center">
-                    <Link href="/analyzer">Resume Analyzer</Link>
-                    <Link href="/analyzer-history">JD Matcher History</Link>
+                    <Link href="/" passHref>
+                        <Button
+                            variant={pathname === "/" ? "default" : "ghost"}
+                            className="text-sm font-normal hover:cursor-pointer"
+                        >
+                            Home
+                        </Button>
+                    </Link>
+                    <Link href="/analyzer" passHref>
+                        <Button
+                            variant={
+                                pathname === "/analyzer" ? "default" : "ghost"
+                            }
+                            className="text-sm font-normal hover:cursor-pointer"
+                        >
+                            Resume Analyzer
+                        </Button>
+                    </Link>
+                    <Link href="/analyzer-history" passHref>
+                        <Button
+                            variant={
+                                pathname === "/analyzer-history"
+                                    ? "default"
+                                    : "ghost"
+                            }
+                            className="text-sm font-normal hover:cursor-pointer"
+                        >
+                            JD Matcher History
+                        </Button>
+                    </Link>
                     <ThemeToggleButton />
                 </div>
             </div>
